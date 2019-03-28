2e69b37f6631ed88ba5dd98211c5657cc6e82a06
Matt Currier <m.currier@premiercoil.com>
2019-03-28 16:50:22 -0500

    re-factor

--- neuron.js
+++ neuron.js
@@ -1,13 +1,21 @@
 let bias = 1
 let net = []
-let prior_inputs = []
 let rate = 1
-let signal = 0
 let size = 3
 let stability = 1
 let threshold = 0.1
 let weight = []
 
+
+function activate() {
+	for (let X = 1; X < size; X++) {	// update signals (top Z-layer)
+		for (let Y = 0; Y < size; Y++){
+			for (let Z = 1; Z <= size; Z++){
+				net[X][Y][0] = Math.max(net[X][Y][Z] * net[X-1][Y][0], 0)	// RELU activation
+			}
+		}
+	}
+}
 
 module.exports = class {
 	constructor(opts={}) {
@@ -30,11 +38,21 @@ module.exports = class {
 		}
 	}
 
+	get control() {
+		let I = Math.floor(size / 2)
+
+		let result = []
+
+		for (let Y = 0; Y < size; Y++) result.push(net[I][Y][0])
+	}
+
 	get print() {
 		let line = ""
 
+		activate()
+
 		console.log()
-		console.log(`==${size}==`)
+		console.log(`=${size}x${size}=`)
 		for (let Z = 0; Z <= size; Z++) {
 			for (let Y = 0; Y < size; Y++) {
 				line = ""
@@ -46,52 +64,58 @@ module.exports = class {
 			}
 			console.log(`---`)
 		}
-		console.log(`==${size}==`)
+		console.log(`=${stability}=`)
 	}
 
-	activate(inputs) {
-		if (typeof inputs == 'object')
-			for (let Y = 0; Y < size; Y++) net[0][Y][0] = inputs[Y]	// glean inputs
-	
-		for (let X = 1; X < size; X++) {	// update signals (top Z-layer)
-			for (let Y = 0; Y < size; Y++){
-				for (let Z = 1; Z <= size; Z++){
-					net[X][Y][0] = Math.max(net[X][Y][Z] * net[X-1][Y][0], 0)	// RELU activation
-				}
-			}
-		}
+	get status() {
+		return stability
 	}
+
+	set inputs(ins) {
+		let inputter = ins.slice()
+
+		for (let Y = 0; Y < size; Y++) net[0][Y][0] = inputter[Y]	// glean inputs
+	}
+
 
 	train(inputter, expectation) {
 		let error = []
 		let expected = expectation.slice()
-		let inputs = []
-		for (let Y = 0; Y < size; Y++) inputs.push(net[0][Y][0])
 
 		stability = 0
-		
+
+		this.inputs = inputter
+
+		activate()
+
 		for (let X = 1; X < size; X++) {
 			for (let Y = 0; Y < size; Y++) {	// count the costs
 				error[Y] = expected[Y] - net[X][Y][0]
 				stability = Math.max(stability, error[Y])
 			}
-			
+
 			for (let Y = 0; Y < size; Y++) {
 				let diff = error[Y] * net[X][Y][0] * rate
-				
+
 				for (let Z = 1; Z <= size; Z++) {
 					net[X][Y][Z] += diff
 				}
 			}
 
-			this.activate(inputter)
-			
+			activate()
+
 			for (let Y = 0; Y < size; Y++)	// glean the expected values for nexties
 			expected[Y] = net[X][Y][0]
 		}
 	}
 
-	learn(ins, outs) {
-		while (stability > threshold) this.train(ins, outs)
+	learn(ins, outs, report=false) {
+		stability = 1
+
+		while (stability > threshold) {
+			this.train(ins, outs)
+		}
+
+		if (report) this.print
 	}
 }
