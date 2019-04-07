b8bc2049000f68c6611c5ed8ad0aca5228d5503b
Matt Currier <mcurrier@pcs.local>
2019-04-06 21:28:41 -0500

    refactor

--- neuron.js
+++ neuron.js
@@ -1,133 +1,67 @@
-let bias = 1
-let net = []
-let rate = 1
-let size = 3
-let stability = 1
-let threshold = 0.1
-let weight = []
-
-
-function activate() {
-	for (let X = 1; X < size; X++) {	// update signals (top Z-layer)
-		for (let Y = 0; Y < size; Y++){
-			for (let Z = 1; Z <= size; Z++){
-				net[X][Y][0] = Math.max(net[X][Y][Z] * net[X-1][Y][0], 0)	// RELU activation
-			}
-		}
-	}
-}
-
 class Neuron {
 	constructor(opts={}) {
-		bias = opts.bias || bias
-		rate = opts.rate || rate
-		size = opts.size || size
-		threshold = opts.threshold || threshold
-		weight = opts.weight || weight
+		this.bias = 1
+		this.net = []
+		this.rate = 0.1
+		this.threshold = 0.1
 
-		for (let X = 0; X < size; X++) {
-			net[X] = new Array(size)
+		this.size = opts.size || 3
+
+		for (let X = 0; X < this.size; X++) {
+			this.net[X] = new Array(this.size)
 			
-			for (let Y = 0; Y < size; Y++){
-				net[X][Y] = new Array(size)
+			for (let Y = 0; Y < this.size; Y++){
+				this.net[X][Y] = new Array(this.size)
 				
-				for (let Z = 0; Z <= size; Z++){
-					net[X][Y][Z] = 0.5
+				for (let Z = 0; Z <= this.size; Z++){
+					this.net[X][Y][Z] = 0.5
 				}
 			}
 		}
 	}
 
-	get control() {
-		let I = Math.floor(size / 2)
-
-		let result = []
-
-		for (let Y = 0; Y < size; Y++) result.push(net[I][Y][0])
-
-		return result
-	}
-	
-	get output() {
-		let result = []
-
-		for (let Y = 0; Y < size; Y++) result.push(net[size-1][Y][0])
-
-		return result
-	}
-
-	get print() {
-		let line = ""
-
-		activate()
-
-		console.log()
-		console.log(`=${size}x${size}=`)
-		for (let Z = 0; Z <= size; Z++) {
-			for (let Y = 0; Y < size; Y++) {
-				line = ""
+	activate() {
+		for (let X = 1; X < this.size; X++) {	// update signals (top Z-layer)
+			for (let Y = 0; Y < this.size; Y++) {
+				this.net[X][Y][0] = 0
 				
-				for (let X = 0; X < size; X++) {
-					line += (Math.round(net[X][Y][Z] * 100) / 100) + "\t"
-				}   
-				console.log(`${line}`)
+				for (let Z = 1; Z <= this.size; Z++) {
+					this.net[X][Y][0] = this.net[X][Y][Z] * this.net[X-1][Y][0]
+				}
+				
+				this.net[X][Y][0] = Math.max(this.net[X][Y][0], 0)	// RELU activation
 			}
-			console.log(`---`)
 		}
-		console.log(`=${stability}=`)
 	}
-
-	get status() {
-		return stability
-	}
-
-	set inputs(ins) {
-		let inputter = ins.slice()
-
-		for (let Y = 0; Y < size; Y++) net[0][Y][0] = inputter[Y]	// glean inputs
-	}
-
 
 	train(inputter, expectation) {
-		let error = []
 		let expected = expectation.slice()
 
-		stability = 0
+		this.inputs = inputter.slice
 
-		this.inputs = inputter
+		for (let Y = 0; Y < this.size; Y++) {	// glean inputs
+			this.net[0][Y][0] = +inputter[Y]
+		}
 
-		activate()
+		this.activate()
 
-		for (let X = 1; X < size; X++) {
-			for (let Y = 0; Y < size; Y++) {	// count the costs
-				error[Y] = expected[Y] - net[X][Y][0]
-				stability = Math.max(stability, error[Y])
-			}
+		for (let X = this.size-1; X > 0; X--) {
+			for (let Y = 0; Y < this.size; Y++) {	// count the costs
+				let error = expected[Y] - this.net[X][Y][0]
 
-			for (let Y = 0; Y < size; Y++) {
-				let diff = error[Y] * net[X][Y][0] * rate
+				let diff = error * this.net[X][Y][0] * this.rate
 
-				for (let Z = 1; Z <= size; Z++) {
-					net[X][Y][Z] += diff
+				for (let Z = 1; Z <= this.size; Z++) {
+					this.net[X][Y][Z] += diff
 				}
 			}
 
-			activate()
+			this.activate()
 
-			for (let Y = 0; Y < size; Y++)	// glean the expected values for nexties
-			expected[Y] = net[X][Y][0]
+			for (let Y = 0; Y < this.size; Y++)	// glean the expected values for nexties
+				expected[Y] = this.net[X][Y][0]
 		}
-	}
-
-	learn(ins, outs, report=false) {
-		stability = 1
-
-		while (stability > threshold) {
-			this.train(ins, outs)
-		}
-
-		if (report) this.print
 	}
 }
 
-// if (module) module.exports = Neuron
+if (module) module.exports = Neuron
